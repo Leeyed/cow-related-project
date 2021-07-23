fce4f4aae97ee9688e75c42a6dcc0a4c401c047e
Leeyed <ls0315@163.com>
2021-07-23 13:27:04 +0800

    add sample

--- Cow_track_demo/gen_video.py
+++ Cow_track_demo/gen_video.py
@@ -10,10 +10,12 @@ fps = 24 #视频每秒24帧
 size = (960, 540) #需要转为视频的图片的尺寸
 #可以使用cv2.resize()进行修改
 
-video = cv2.VideoWriter("VideoTest1.avi", cv2.VideoWriter_fourcc('I', '4', '2', '0'), fps, size)
+fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+# video = cv2.VideoWriter("VideoTest1.mp4", cv2.VideoWriter_fourcc('I', '4', '2', '0'), fps, size)
+video = cv2.VideoWriter("sample.mp4", fourcc , fps, size)
 #视频保存在当前目录下
 
-for index in range(5594):
+for index in range(1000, 2000, 3):
 # for index in range(100):
     img = os.path.join(path, str(index+1)+'.jpg')
     print(img)
